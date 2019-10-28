d0de75a1823a7d8eadb4bedd825eccf5e79f52a7
MarinMalouin <p5oc@free.fr>
2019-10-28 20:26:32 +0100

    Covered
    At > 70%

---
+++ tests/Controller/TaskControllerTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class TaskControllerTest extends WebTestCase
+{
+    public function testListAction()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks');
+
+        $this->assertSame("Liste des tâches en cours", $crawler->filter('h2')->text());
+    }
+
+    public function testListActionFinished()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks');
+
+        $link = $crawler->selectLink('Accéder à la liste des tâches terminés')->link();
+        $crawler = $client->click($link);
+
+        $this->assertSame("Liste des tâches terminées", $crawler->filter('h2')->text());
+    }
+
+    public function testCreatePage()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    
+    public function testCreateTask()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['task[title]'] = 'Test';
+        $form['task[content]'] = 'Ceci est un article de test';
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+
+    public function testEditPage()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks');
+
+        $link = $crawler->selectLink('Test')->link();
+        $crawler = $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+
+    public function testEditTask()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks');
+
+        $link = $crawler->selectLink('Test')->link();
+        $crawler = $client->click($link);
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['task[title]'] = 'Test';
+        $form['task[content]'] = 'Ceci est un article de test modifié';
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testToggleTask()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks');
+
+        $id = $crawler->filter("#taskTest p")->text();
+
+        $crawler = $client->request('GET', '/tasks/' . $id . '/toggle');
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+
+    public function testDeleteTask()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/tasks-finished');
+
+        $id = $crawler->filter("#taskTest p")->text();
+
+        $crawler = $client->request('DELETE', '/tasks/' . $id . '/delete');
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+}
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -6,24 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function clientConnected()
+    public function testListAction()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
 
-        $form = $crawler->selectButton('Se connecter')->form();
-        $form['_username'] = 'test';
-        $form['_password'] = 'test';
-        $crawler = $client->submit($form);
+        $link = $crawler->selectLink('Utilisateurs')->link();
+        $crawler = $client->click($link);
 
-        $crawler = $client->followRedirect();
-
-        return ['client' => $client, 'crawler' => $crawler];
+        $this->assertSame("Liste des utilisateurs", $crawler->filter('h1')->text());
     }
 
-    public function testCreateUserPage()
+    public function testCreatePage()
     {
-        $array = $this->clientConnected();
+        $array = Connexion::connexion();
         $client = $array['client'];
         $crawler = $array['crawler'];
 
@@ -32,4 +29,69 @@ class UserControllerTest extends WebTestCase
 
         $this->assertSame("Créer un utilisateur", $crawler->filter('h1')->text());
     }
+
+    public function testCreateUser()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/users/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['user[username]'] = 'demo';
+        $form['user[password][first]'] = 'demo';
+        $form['user[password][second]'] = 'demo';
+        $form['user[email]'] = 'demo@demo.fr';
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+
+        $crawler = $client->request('GET', '/users');
+
+        $id = $crawler->filter("#user-demo p")->text();
+
+        $crawler = $client->request('DELETE', '/users/' . $id . '/delete');
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+    }
+
+    public function testEditUser()
+    {
+        $array = Connexion::connexion();
+        $client = $array['client'];
+        $crawler = $array['crawler'];
+
+        $crawler = $client->request('GET', '/users/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['user[username]'] = 'demo';
+        $form['user[password][first]'] = 'demo';
+        $form['user[password][second]'] = 'demo';
+        $form['user[email]'] = 'demo@demo.fr';
+        $crawler = $client->submit($form);
+
+        //$crawler = $client->followRedirect();
+
+        $crawler = $client->request('GET', '/users');
+
+        $id = $crawler->filter("#user-demo p")->text();
+
+        $crawler = $client->request('DELETE', '/users/' . $id . '/edit');
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['user[email]'] = 'demo@demo.com';
+        $crawler = $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+
+        $crawler = $client->request('DELETE', '/users/' . $id . '/delete');
+    }
+
 }
