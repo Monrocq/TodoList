1a6856dc8757ba496d74a8fcf3a92c41a1c37a2a
MarinMalouin <p5oc@free.fr>
2019-11-24 11:34:36 +0100

    Begin cache

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response as Response;
 
 class DefaultController extends AbstractController
 {
@@ -12,6 +13,16 @@ class DefaultController extends AbstractController
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $response = $this->render('default/index.html.twig'); /* @var $response Response */
+        $response->setSharedMaxAge(800);
+        $date = new \DateTime();
+        $date->modify('+800 seconds');
+        $response->setExpires($date);
+
+        //var_dump( get_class_methods('Symfony\Component\HttpFoundation\Response'));die;
+
+        $response->headers->addCacheControlDirective('must-revalidate', false);
+        return $response;
+
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Services\CacheService;
+use Psr\Cache\CacheException;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class UserController extends AbstractController
 {
@@ -23,7 +27,8 @@ class UserController extends AbstractController
      */
     public function listAction()
     {
-        return $this->render('user/list.html.twig', ['users' => $this->getDoctrine()->getRepository('App:User')->findAll()]);
+
+        return $this->render('user/list.html.twig', ['users' => $result]);
     }
 
     /**
@@ -89,5 +94,113 @@ class UserController extends AbstractController
         $this->addFlash('success', 'L\'utilisateur a bien été supprimé.');
 
         return $this->redirectToRoute('user_list');
+    }
+
+    /**
+     * Returns the key for the current cache item.
+     *
+     * The key is loaded by the Implementing Library, but should be available to
+     * the higher level callers when needed.
+     *
+     * @return string
+     *   The key string for this cache item.
+     */
+    public function getKey()
+    {
+        // TODO: Implement getKey() method.
+    }
+
+    /**
+     * Confirms if the cache item lookup resulted in a cache hit.
+     *
+     * Note: This method MUST NOT have a race condition between calling isHit()
+     * and calling get().
+     *
+     * @return bool
+     *   True if the request resulted in a cache hit. False otherwise.
+     */
+    public function isHit()
+    {
+        // TODO: Implement isHit() method.
+    }
+
+    /**
+     * Sets the value represented by this cache item.
+     *
+     * The $value argument may be any item that can be serialized by PHP,
+     * although the method of serialization is left up to the Implementing
+     * Library.
+     *
+     * @param mixed $value
+     *   The serializable value to be stored.
+     *
+     * @return static
+     *   The invoked object.
+     */
+    public function set($value)
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param \DateTimeInterface|null $expiration
+     *   The point in time after which the item MUST be considered expired.
+     *   If null is passed explicitly, a default value MAY be used. If none is set,
+     *   the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAt($expiration)
+    {
+        // TODO: Implement expiresAt() method.
+    }
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param int|\DateInterval|null $time
+     *   The period of time from the present after which the item MUST be considered
+     *   expired. An integer parameter is understood to be the time in seconds until
+     *   expiration. If null is passed explicitly, a default value MAY be used.
+     *   If none is set, the value should be stored permanently or for as long as the
+     *   implementation allows.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAfter($time)
+    {
+        // TODO: Implement expiresAfter() method.
+    }
+
+    /**
+     * Adds a tag to a cache item.
+     *
+     * Tags are strings that follow the same validation rules as keys.
+     *
+     * @param string|string[] $tags A tag or array of tags
+     *
+     * @return $this
+     *
+     * @throws InvalidArgumentException When $tag is not valid
+     * @throws CacheException           When the item comes from a pool that is not tag-aware
+     */
+    public function tag($tags): self
+    {
+        // TODO: Implement tag() method.
+    }
+
+    /**
+     * Returns a list of metadata info that were saved alongside with the cached value.
+     *
+     * See ItemInterface::METADATA_* consts for keys potentially found in the returned array.
+     */
+    public function getMetadata(): array
+    {
+        // TODO: Implement getMetadata() method.
     }
 }
